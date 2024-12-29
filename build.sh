7847bcf44b4d414b71c165b1118d9827dd400463
Kevin Tran <kevin.tran@parker.com>
2024-12-28 17:18:35 -0800

    asdf

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ mkdir -p reticle.app/Contents/MacOS
 mkdir -p reticle.app/Contents/Resources
 mkdir -p ~/Library/Application\ Support/reticle
 mkdir -p reticle.app/Contents/Resources/Assets
+
 cp -R Assets/* reticle.app/Contents/Resources/Assets/
 cp config.json ~/Library/Application\ Support/reticle/
 cp Info.plist reticle.app/Contents/
+
 swiftc -o reticle.app/Contents/MacOS/reticle Sources/*.swift
 
 
