e63a81595a35166ff4dd143d203e42d7470aeee0
Kevin Tran <kevin.tran@parker.com>
2025-01-01 17:32:01 -0800

    misc code

--- Sources/WindowFactory.swift
+++ Sources/WindowFactory.swift
@@ -13,12 +13,15 @@ class WindowFactory {
         )
         window.isOpaque = false
         window.backgroundColor = NSColor.clear
+        // window.backgroundColor = NSColor.red
+        // window.alphaValue = 0.5
         window.level = .screenSaver  // Ensure it stays above all UI elements
         window.ignoresMouseEvents = true
         window.collectionBehavior = [
-            .canJoinAllSpaces,
-            .fullScreenAuxiliary,
-            .ignoresCycle
+            .canJoinAllSpaces, // ensures available on all desktops
+            .fullScreenAuxiliary, // keeps window visible in full-screen apps
+            .stationary, // prevents from interacting with Mission Control
+            .ignoresCycle // excludes window from normal app window cycles
         ]
         window.makeKeyAndOrderFront(nil)
         return window
